dd6118d69a97be518774c7e3538ed07c4331208b
nicha.satt <nicha.satt@gmail.com>
2022-06-08 18:26:22 +0700

    search -display search result 1 (make result)

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-function showGenre() {
+
+
+
+function getAllAnimeByGenre() { //sort by genre
+    let page = 1
+    fetch(`https://api.jikan.moe/v4/anime?page=${page}`)
+    .then(response => {
+        return response.json()
+    }) 
+    .then(data => {
+        console.log('success', data.data[0].genres)
+    })
+}
+
+
+function getGenre() {  //get all anime genre
     fetch('https://api.jikan.moe/v4/genres/anime')
     .then(response => {
         return response.json()
     }) 
     .then(data => {
-        console.log('success', data)
+        //console.log('success', data)
         showGenreEach(data.data)
     })
 }
-
 function showGenreEach(data) {
     const genres = []
     const genresName = []
-    for (genre of data) {
+    for (let genre of data) {
         let genreName = genre.name
         let genreCount = genre.count
 
@@ -29,13 +43,15 @@ function showGenreEach(data) {
         addGenre(each)
     }
 }
-function addGenre(genre) { 
+function addGenre(genre) { // home - show categories
     const genreGroup = document.getElementById('genre')
     let box = document.createElement('div')
     box.classList.add('card')
     box.classList.add('d-flex')
     box.classList.add('text-center')
-    box.classList.add('col-3')
+    box.classList.add('col-12')
+    box.classList.add('col-sm-6')
+    box.classList.add('col-md-4')
     box.classList.add('border-info')
 
 
@@ -55,10 +71,21 @@ function addGenre(genre) {
 
     box.appendChild(boxBody)
 
+    box.addEventListener('click', () => {
+        console.log(genre.name)
+        //getAllAnimeByGenre()
+    })
+
     genreGroup.appendChild(box)
 }
 
+document.getElementById('search').addEventListener('click', () => {
+    document.getElementById('categories').classList.add('visually-hidden')
+    console.log('ok')
+    
+})
+
 function onLoad() {
-    showGenre()
+    getGenre()
 }
 window.addEventListener('load', onLoad)
